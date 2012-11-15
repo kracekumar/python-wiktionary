84d296e1b6425794375d4d319fb76f66645c8f2f
kracekumar <me@kracekumar.com>
2012-11-16 01:51:02 +0530

    modified m2wsgi

--- m2wsgi_server.py
+++ m2wsgi_server.py
@@ -3,8 +3,8 @@
 
 
 from m2wsgi.io.standard import WSGIHandler
-from mysuperapp import wsgiapp
+from wiktionary import app
 
 zmq_port = "tcp://127.0.0.1:9999"
-handler = WSGIHandler(wsgiapp, zmq_port)
+handler = WSGIHandler(app, zmq_port)
 handler.serve()
