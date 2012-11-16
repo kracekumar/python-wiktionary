6593b0fa0581231dbf6dadb5dbf36dce26636842
kracekumar <me@kracekumar.com>
2012-11-16 18:36:49 +0530

    Added support for env variable

--- runserver.py
+++ runserver.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 #! -*- coding: utf-8 -*-
 
+import os
 import sys
 from wiktionary import app, init
 from wiktionary.models import db
+
 
 try:
     app.config.from_pyfile('settings.py')
@@ -16,6 +18,9 @@ if app.config['ENVIRONMENT'] == u'dev':
     app.run(app.config['IP'], app.config['PORT_NO'], debug=True)
 elif app.config['ENVIRONMENT'] == u'gevent':
     from gevent import wsgi
+    port = int(os.getenv("PORT"))
+    if port:
+        app.config['PORT_NO'] = port
     server = wsgi.WSGIServer((app.config['IP'], app.config['PORT_NO']), app)
     server.serve_forever()
 else:
